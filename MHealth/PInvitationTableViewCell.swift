001887ab30be975ba0df4d7e112ea03a5ad24cc9
أنا <ahmad9317@hotmail.com>
2017-04-02 20:47:36 +0300

    create custom cell for patient request

---
+++ MHealth/PInvitationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PInvitationTableViewCell.swift
+//  MHealth
+//
+//  Created by Ahmad alkandari on 4/2/17.
+//  Copyright © 2017 PIFSS. All rights reserved.
+//
+
+import UIKit
+
+class PInvitationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
