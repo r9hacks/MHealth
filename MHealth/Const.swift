6e55a22eab46cd92d82bbbe172dab4f7a3189cf1
Entisar <iamentisar@gmail.com>
2017-04-02 16:45:50 +0300

    add the links

--- MHealth/Const.swift
+++ MHealth/Const.swift
@@ -35,4 +35,38 @@ class Const: NSObject {
         static let drProfile = "drProfile"
         
     }
+    
+    
+    /*
+     public static String BASE = "http://34.196.107.188:8081/MhealthWeb/webresources/";
+     
+     public static String Doctor = BASE + "doctor";
+     public static String PatientDrLink = BASE + "patientdrlink";
+     
+     public static String getDoctor = Doctor + "/getDoctor/";
+     
+     public static String Patient = BASE + "patient";
+     public static String PatientReport = BASE + "patientreport";
+     
+     public static String resetPassword = Doctor + "/reset/";
+     public static String login = Doctor + "/login/";
+     
+     
+     public static String PendingRequest = Doctor + "/pendingdoctor/";
+     public static String MyPatient = Doctor + "/accepteddoctor/";
+     public static String GetRepliedReport = PatientReport + "/getReports/";
+     public static String ReplyReport = PatientReport +"/UpdatePatientReportDRec/";
+     public static String GetPatientReport = PatientReport +"/getPatientReport/";
+     
+     
+     public static String PrefDoctorProfile = "doctorProfile";
+     public static String PrefReport = "PatientReport";
+     
+     public static String PrefPatientProfile = "patientProfile";
+     public static String PrefLanguage = "APPLanguage";
+     
+     public static String UpdateRequestStatus = PatientDrLink + "/setstatus/";
+     */
+    
+    
 }
