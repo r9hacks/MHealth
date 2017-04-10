ba539a1b0965a73386e619da9a55ed9789b7f6ff
أنا <ahmad9317@hotmail.com>
2017-04-10 10:12:20 +0300

    blood request cell

---
+++ MHealth/BloodRequestCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BloodRequestCellTableViewCell.swift
+//  MHealth
+//
+//  Created by Ahmad alkandari on 4/10/17.
+//  Copyright © 2017 PIFSS. All rights reserved.
+//
+
+import UIKit
+
+class BloodRequestCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
